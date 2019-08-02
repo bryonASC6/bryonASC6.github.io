8a2ce1f7ee0bcb6d96dae6fcf86ba44c645e77c5
bryonASC6 <bphil622@gmail.com>
2019-08-02 13:41:27 -0400

    add website

---
+++ Bryon.js
@@ -0,0 +1,10 @@
+//get references
+
+//declare an array (with different links in it)
+
+let gamesArray=[]
+
+// generate a random #
+let randomSite= parseInt(Math.random())
+//use the random # + array to pick the random link
+
